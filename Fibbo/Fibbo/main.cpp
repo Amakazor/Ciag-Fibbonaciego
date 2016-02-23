17cdd009c7870cbb3d99975a1235a4b44a2b8bc5
Amakazor <Amakazor@gmail.com>
2016-02-23 20:14:41 +0100

    Constructed simple, but functional code

--- Fibbo/Fibbo/main.cpp
+++ Fibbo/Fibbo/main.cpp
@@ -4,8 +4,43 @@ using namespace std;
 
 int main()
 {
+	int u_input;
+	int output;
 
-
+	cout << "=========================" << endl;
+	cout << "Witaj!" << endl << "Ten program pokaze Ci wybrany wyraz Ciagu Fibbonaciego" << endl;
+	cout << "=========================" << endl;
+	cout << endl << "Ktory wyraz ciagu chcesz zobaczyc?" << endl;
+	cin >> u_input;
+	if (u_input == 0)
+	{
+		cout << "Ciag nie posiada zerowego wyrazu";
+	}
+	else
+	{
+		if ((u_input == 1) || (u_input == 2))
+		{
+			output = 1;
+		}
+		else
+		{
+			int l1 = 1, l2 = 1;
+			for (int i = 3; i <= u_input; i = i + 2)
+			{
+				l1 = l1 + l2;
+				l2 = l2 + l1;
+			};
+			if (u_input	% 2 == 0)
+			{
+				output = l2;
+			}
+			else
+			{
+				output = l1;
+			};
+		};
+		cout << u_input << " wyraz Ciagu Fibbonaciego to: " << output;
+	};
 	system("pause");
 	return 0;
 };
